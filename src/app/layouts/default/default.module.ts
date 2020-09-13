a632afa7bdb012a50def7fae4f6e3d7291d03f2c
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-09-13 18:17:59 +0530

    Added MatSidenavModule

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.componen
 import { RouterModule } from '@angular/router';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     RouterModule,
-    SharedModule
+    SharedModule,
+    MatSidenavModule
   ]
 })
 export class DefaultModule { }
