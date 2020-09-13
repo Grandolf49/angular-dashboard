9674afe500c9e3de9c19ca9cf952954aef071466
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-09-13 18:02:21 +0530

    Added header, footer, sidebar, posts components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
 
 const routes: Routes = [{
@@ -10,6 +11,10 @@ const routes: Routes = [{
   children: [{
     path: '',
     component: DashboardComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
   }]
 }];
 
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
 
 @NgModule({
   declarations: [
     DefaultComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostsComponent
   ],
   imports: [
     CommonModule,
