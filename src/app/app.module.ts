9411719e1d30e06e288d2a3c884f46d1fd4fe87f
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-09-13 15:05:00 +0530

    Added dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
