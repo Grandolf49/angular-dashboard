a90473c2fb9ec1d00023043492f6b241a85cef17
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-09-14 16:37:24 +0530

    Added list items in sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,6 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     FlexLayoutModule,
     MatMenuModule,
-    MatListModule
+    MatListModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
