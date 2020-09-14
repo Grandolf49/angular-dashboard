6111dc52e3e9ba7221caac38f2c3cb535b771e0b
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-09-14 16:21:48 +0530

    Added placeholders for dashboard content

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     CommonModule,
     RouterModule,
     SharedModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatDividerModule
   ]
 })
 export class DefaultModule { }
