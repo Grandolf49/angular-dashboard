154cacccd138de757a6979a435adc7cd7fdb7b21
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-09-18 01:17:37 +0530

    Added area graph for card component

--- src/app/shared/widgets/card/card.component.ts
+++ src/app/shared/widgets/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as HighCharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
 
 @Component({
   selector: 'app-widget-card',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  highCharts = HighCharts
+  chartOptions = {}
+
   constructor() { }
 
   ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+        type: 'area'
+      },
+      title: {
+        text: 'Demo'
+      },
+      subtitle: {
+        text: 'Source: Wikipedia.org'
+      },
+      xAxis: {
+        categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+        tickmarkPlacement: 'on',
+        title: {
+          enabled: false
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Billions'
+        },
+        labels: {
+          formatter: function () {
+            return this.value / 1000;
+          }
+        }
+      },
+      tooltip: {
+        split: true,
+        valueSuffix: ' millions'
+      },
+      plotOptions: {
+        area: {
+          stacking: 'normal',
+          lineColor: '#666666',
+          lineWidth: 1,
+          marker: {
+            lineWidth: 1,
+            lineColor: '#666666'
+          }
+        }
+      },
+      series: [{
+        data: [71, 70, 30, 66]
+      }]
+    };
+
+    HC_exporting(HighCharts);
   }
 
 }
